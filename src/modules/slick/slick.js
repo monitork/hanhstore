75dc0f2d9d91fbaa8ecd6d0cd63b8423822e4c66
Nguyen Toai <nguyentoai.dev@gmail.com>
2020-10-19 23:40:08 +0700

    [ADD] ...

--- src/modules/slick/slick.js
+++ src/modules/slick/slick.js
@@ -4,12 +4,19 @@ import 'slick-carousel/slick/slick.css'
 
 export default function slick() {
     const slick = document.querySelector('[data-js="slick"]');
+    const bgWidth = $(window).width();
+    const bgHeight = $(window).width() / 3;
     if (slick) {
         slick.classList.remove('is-hidden')
         slick.offsetHeight;
         const slickOption = {
             autoplay: true,
-            speed: 300
+            speed: 300,
+            dots: true,
+            infinite: true,
+            arrows: false,
+            width: bgWidth,
+            height: bgHeight
         }
         $(slick).slick(slickOption)
     }
